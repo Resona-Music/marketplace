d279d944ca7a9125e828240bed210a4af72506b2
Nevflorals <nevaehhourican@icloud.com>
2025-11-02 15:22:13 +0000

    Setup Neon Postgres with Drizzle

---
+++ drizzle/0000_thick_hex.sql
@@ -0,0 +1,11 @@
+CREATE TABLE "users" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"username" varchar(255) NOT NULL,
+	"email" varchar(255) NOT NULL,
+	"password" varchar(255) NOT NULL,
+	"role" varchar(50) DEFAULT 'user' NOT NULL,
+	"createdAt" timestamp DEFAULT now() NOT NULL,
+	"updatedAt" timestamp DEFAULT now() NOT NULL,
+	CONSTRAINT "users_username_unique" UNIQUE("username"),
+	CONSTRAINT "users_email_unique" UNIQUE("email")
+);
